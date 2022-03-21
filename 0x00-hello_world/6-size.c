554f8029a75fd8cd25eb493a287b89df8a7b5bcb
mjt1 <mercylynetuwei@gmail.com>
2022-03-21 12:08:37 +0000

    My commit fo 6-size.c task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/*
+ * main - Entry
+ *
+ * Return: Always 0 (Success);
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of char: %d byte(s)\n", sizeof(d));
+        printf("Size of int: %d byte(s)\n, sizeof(a));
+	printf("Size of a long int: %d byte(s)\n", sizeof(b));
+	printf("Size of float: %d byte(s)\n", sizeof(f));
+        printf("Size of long long int: %d byte(s)\n", size of(c));
+        return (0);
+}       
